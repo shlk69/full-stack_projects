d1f387d9bb80ac337d515bb3c7e7d8b2fb12a577
shlk69 <sahilkurmi356@gmail.com>
2026-06-02 16:55:29 +0530

    Email sending functionality complete

---
+++ real-thoughts/emails/VerificationEmail.tsx
@@ -0,0 +1,57 @@
+import {
+  Font,
+  Head,
+  Html,
+  Heading,
+  Preview,
+  Section,
+  Row,
+  Text,
+} from "@react-email/components";
+import { exportPages } from "next/dist/export/worker";
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail  ({ username, otp }: VerificationEmailProps)  {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification Code</title>
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://gstatic.com",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Preview>Here&apos;s your verification code: {otp}</Preview>
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username},</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for registering. Please use the following verification
+            code to complete your registration:
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            If you did not request this code, please ignore this email.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+};
+
