8457c5bb71a42b25c96a8a5c2f338b37fc374550
shlk69 <sahilkurmi356@gmail.com>
2026-06-08 14:50:15 +0530

    frontend setup complete

---
+++ echoCHAT/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import {Route,Routes} from 'react-router'
+
+export default function App() {
+  return (
+    <div
+      className="flex h-screen items-center justify-center bg-base-100 text-base-content"
+      data-theme="night">
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
---
+++ echoCHAT/frontend/src/pages/OnboarfingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OnboarfingPage = () => {
+  return (
+    <div>OnboarfingPage</div>
+  )
+}
+
+export default OnboarfingPage
