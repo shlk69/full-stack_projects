c8fe5f63c7a939975703552714454f5dbde168ee
shlk69 <sahilkurmi356@gmail.com>
2026-06-14 10:03:52 +0530

    theme selector complete

--- echoCHAT/frontend/src/App.jsx
+++ echoCHAT/frontend/src/App.jsx
@@ -14,8 +14,10 @@ import {Toaster} from 'react-hot-toast'
 import PageLoader from "./components/PageLoader.jsx";
 import { getAuthUser } from "./lib/api.js";
 import useAuthUser from "./hooks/useAuthUser.js";
+import useThemeStore from "./store/useThemeStore.js";
 
 const App = () => {
+  const {theme} = useThemeStore()
    
   const { isLoading, authUser } = useAuthUser()
   
@@ -25,7 +27,7 @@ const App = () => {
   if(isLoading) return <PageLoader/>
 
   return (
-    <div className="h-screen" data-theme="forest">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
