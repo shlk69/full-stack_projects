3bc1a2177a513e81ff79172520cf9b49f4cb1e5f
shlk69 <sahilkurmi356@gmail.com>
2026-06-22 19:34:48 +0530

    tailwind configured

--- AIdev/client/vite.config.js
+++ AIdev/client/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    },
+    proxy: {
+      '/cdn': {
+        target: 'https://unpkg.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cdn/, '')
+      }
+    }
+  }
 })
